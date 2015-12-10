7c0f22847e81352aa596bfb7b5311f9558e76c59
warrendodsworth <warren.dodsworth@gmail.com>
2015-12-10 15:36:37 +1100

    Setup Gulp

--- Api/gulpfile.js
+++ Api/gulpfile.js
@@ -1,67 +1,80 @@
 /// <vs AfterBuild='default' SolutionOpened='default' />
 //https://github.com/JustMaier/angular-signalr-hub - IMP
 
-
 var gulp = require('gulp');
 var bower = require('gulp-bower');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 
 var project = { webroot: '' };
 var paths = {
-  webroot: "./" + project.webroot + "/",
-  bower: "./bower_components/",
-  lib: "./" + project.webroot + "/lib/"
+  webroot: './',
+  bower: './bower_components/',
+  lib: './lib/'
 };
 
-gulp.task('default', function () {
-  // place code for your default task here
-});
+gulp.task('default', ['watch', 'lib', 'src']);
 
-
-
-gulp.task("copy", ["clean"], function () {
+gulp.task("lib", function () {
   var bower = {
+    "angular": "angular/**/*.min.{js,css}",
+    "angular": "angular-bootstrap/**/*.min.{js,css}", //Not getting copied
+    "angular": "angular-local-storage/dist/**/*.min.{js,css}",
     "bootstrap": "bootstrap/dist/**/*.{js,map,css,ttf,svg,woff,eot}",
-    "bootstrap-touch-carousel": "bootstrap-touch-carousel/dist/**/*.{js,css}",
-    "hammer.js": "hammer.js/hammer*.{js,map}",
-    "jquery": "jquery/jquery*.{js,map}",
-    "jquery-validation": "jquery-validation/jquery.validate.js",
-    "jquery-validation-unobtrusive": "jquery-validation-unobtrusive/jquery.validate.unobtrusive.js"
   }
 
   for (var destinationDir in bower) {
-    gulp.src(paths.bower + bower[destinationDir])
-      .pipe(gulp.dest(paths.lib + destinationDir));
+    gulp.src(paths.bower + bower[destinationDir]).pipe(gulp.dest(paths.lib + destinationDir));
   }
-});
 
-//Runs bower install
-gulp.task('bower', function () {
-  return bower({ layout: "byComponent" }).pipe(gulp.dest('lib/'));
+  gulp.src('./lib/**/*.js')
+      .pipe(concat('lib.js'))
+      .pipe(rename({ suffix: '.min' }))
+      .pipe(uglify())
+      .pipe(gulp.dest('./lib'));
 
+  gulp.src('./lib/**/*.css')
+         .pipe(concat('lib.css'))
+         .pipe(rename({ suffix: '.min' }))
+         .pipe(minifyCss())
+         .pipe(gulp.dest('./lib'));
 });
 
 // Concatenate JS Files
-gulp.task('scripts', function () {
-  return gulp.src('/js/*.js')
-             .pipe(concat('main.js'))
-             .pipe(rename({ suffix: '.min' }))
-             .pipe(uglify())
-             .pipe(gulp.dest('/js/src'));
+gulp.task('src', function () {
+  gulp.src('./js/**/*.js')
+        .pipe(concat('src.js'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(uglify())
+        .pipe(gulp.dest('./js'));
+
+  gulp.src('./content/**/*.css')
+        .pipe(concat('src.css'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(minifyCss())
+        .pipe(gulp.dest('./css'));
 });
 
+
+//Watch
 gulp.task('watch', function () {
-  // Watch .js files
-  gulp.watch('/js/*.js', ['scripts']);
-  // Watch .scss files
+
+  gulp.watch('/js/*.js', ['src']);
+
+  // recompile and minify
   gulp.watch('/scss/*.scss', ['sass']);
-  // Watch image files
-  gulp.watch('/images/**/*', ['images']);
 });
 
-gulp.task('default', ['watch']);
 
 
 
+
+
+
+//"jquery": "jquery/jquery*.{js,map}",
+//"bootstrap-touch-carousel": "bootstrap-touch-carousel/dist/**/*.{js,css}",
+//"hammer.js": "hammer.js/hammer*.{js,map}",
+//"jquery-validation": "jquery-validation/jquery.validate.js",
+//"jquery-validation-unobtrusive": "jquery-validation-unobtrusive/jquery.validate.unobtrusive.js"
