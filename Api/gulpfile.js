c2157c14caf1929d8a4bbe72ad8b7897d26cdf79
Warren Dodsworth <warrendodsworth@outlook.com>
2016-01-28 21:01:54 +1100

    Gulp config

--- Api/gulpfile.js
+++ Api/gulpfile.js
@@ -13,31 +13,21 @@ var plugins = require("gulp-load-plugins")({
 });
 
 // Define default destination folder
-var dest = './www/';
 var paths = {
     sass: ['./scss/**/*.scss'],
-    cssFiles: ['./www/css/**/*.css'],
-    jsFiles: ['./www/**/*.js', '!./www/lib/**/*.js', '!./www/js/test/**/*.js']
+    css: ['./www/css/**/*.css'],
+    js: ['./www/**/*.js', '!./www/lib/**/*.js', '!./www/js/test/**/*.js']
 };
 
 gulp.task('default', ['src', 'lib', 'watch']);
 
 gulp.task('src', function () {
-    var js = gulp.src(paths.jsFiles)
+    gulp.src(paths.js)
         .pipe(plugins.concat('src.js'))
-        .pipe(gulp.dest(dest + 'js'))
+        .pipe(gulp.dest('./www/js'))
         .pipe(plugins.rename({ suffix: '.min' }))
         .pipe(plugins.uglify())
-        .pipe(gulp.dest(dest + 'js'));
-
-    var css = gulp.src(paths.cssFiles)
-        .pipe(plugins.concat('src.css'))
-        .pipe(gulp.dest(dest + 'css'))
-        .pipe(plugins.rename({ suffix: '.min' }))
-        .pipe(plugins.minifyCss())
-        .pipe(gulp.dest(dest + 'css'));
-
-    return plugins.merge(js, css);
+        .pipe(gulp.dest( './www/js'));
 });
 
 gulp.task('lib', function () {
@@ -45,32 +35,66 @@ gulp.task('lib', function () {
         .pipe(plugins.filter('*.js'))
         .pipe(plugins.concat('lib.min.js'))
         .pipe(plugins.uglify())
-        .pipe(gulp.dest(dest + 'js'));
+        .pipe(gulp.dest('./www/js'));
 
     var css = gulp.src(plugins.mainBowerFiles())
         .pipe(plugins.filter('*.css'))
         .pipe(plugins.concat('lib.min.css'))
         .pipe(plugins.minifyCss())
-        .pipe(gulp.dest(dest + 'css'));
+        .pipe(gulp.dest('./www/css'));
 
     return plugins.merge(js, css);
 });
 
-gulp.task('sass', function(done) {
-  gulp.src(paths.sass)
-    .pipe(plugins.sass())
-    .on('error', plugins.sass.logError)
-    .pipe(gulp.dest('./www/css/'))
-    .pipe(plugins.minifyCss({
-      keepSpecialComments: 0
-    }))
-    .pipe(plugins.rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
-    .on('end', done);
+gulp.task('sass', function (done) {
+    gulp.src(paths.sass)
+        .pipe(plugins.sass())
+        .on('error', plugins.sass.logError)
+        .pipe(gulp.dest('./www/css/'))
+        .pipe(plugins.minifyCss({
+            keepSpecialComments: 0
+        }))
+        .pipe(plugins.rename({ extname: '.min.css' }))
+        .pipe(gulp.dest('./www/css/'))
+        .on('end', done);
 });
 
 gulp.task('watch', function () {
-    gulp.watch(paths.jsFiles, ['src']);
-    gulp.watch(paths.cssFiles, ['src']);
+    gulp.watch(paths.js, ['src']);
+    gulp.watch(paths.css, ['src']);
     gulp.watch(paths.sass, ['sass']);
 });
+
+
+
+gulp.task('install', ['git-check'], function () {
+    return plugins.bower.commands.install()
+        .on('log', function (data) {
+            plugins.gutil.log('bower', plugins.gutil.colors.cyan(data.id), data.message);
+        });
+});
+
+gulp.task('git-check', function (done) {
+    if (!plugins.sh.which('git')) {
+        console.log(
+            '  ' + gutil.colors.red('Git is not installed.'),
+            '\n  Git, the version control system, is required to download Ionic.',
+            '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
+            '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
+            );
+        process.exit(1);
+    }
+    done();
+});
+
+
+/*
+    var css = gulp.src(paths.css)
+        .pipe(plugins.concat('src.css'))
+        .pipe(gulp.dest(dest + 'css'))
+        .pipe(plugins.rename({ suffix: '.min' }))
+        .pipe(plugins.minifyCss())
+        .pipe(gulp.dest(dest + 'css'));
+
+    return plugins.merge(js, css);
+*/
