738f96fe75ac3d8d225a4a839964adb4aa025b2a
Warren Dodsworth <warrendodsworth@outlook.com>
2016-01-31 19:43:25 +1100

    Cleanup

--- Api/www/app.js
+++ Api/www/app.js
@@ -11,25 +11,24 @@
 
     angular.module('controllers', ['services']);
 
+    angular.module('directives', ['services']);
+
     angular.module('services', []);
 
     //Plugins
     angular
        .module('app')
-       .config(['$httpProvider', function ($httpProvider) {
+       .config(['$httpProvider','$facebookProvider', function ($httpProvider, $facebookProvider) {
            $httpProvider.interceptors.push('AuthInterceptorService');
-       }])
-       .config(['$facebookProvider', function ($facebookProvider) {
-           $facebookProvider.setAppId('292179600807388')
+           
+              $facebookProvider.setAppId('292179600807388')
                             .setPermissions("email,user_likes");
        }])
        
-      .run(['AccountService', function (AccountService) {
+      .run(['$rootScope','AccountService', function ($rootScope, AccountService) {
           AccountService.getIdentity();
-      }])
-      .run(function ($rootScope) {
-          // Cut and paste the "Load the SDK" code from the facebook javascript sdk page.
 
+          // Cut and paste the "Load the SDK" code from the facebook javascript sdk page.
           // Load the facebook SDK asynchronously
           (function (d, s, id) {
               var js, fjs = d.getElementsByTagName(s)[0];
@@ -38,7 +37,7 @@
               js.src = "//connect.facebook.net/en_US/sdk.js";
               fjs.parentNode.insertBefore(js, fjs);
           }(document, 'script', 'facebook-jssdk'));
-      });
+      }]);
 
 
     //Routes
