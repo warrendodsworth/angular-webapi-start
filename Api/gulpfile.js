4072017ff3547f8438ef080bf781a27cd40b3574
warrendodsworth <warren.dodsworth@gmail.com>
2016-02-04 12:24:29 +1100

    Gulp setup

--- Api/gulpfile.js
+++ Api/gulpfile.js
@@ -1,86 +1,89 @@
 /// <binding AfterBuild='lib' Clean='default' ProjectOpened='default, watch' />
 /// <vs AfterBuild='lib' SolutionOpened='default' />
-//https://github.com/JustMaier/angular-signalr-hub
-//http://andy-carter.com/blog/a-beginners-guide-to-package-manager-bower-and-using-gulp-to-manage-components
 
-// Include Gulp
 var gulp = require('gulp');
-var mergeStream = require('merge-stream');
+var gutil = require('gulp-util');
+var bower = require('bower');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var minifyCss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var filter = require('gulp-filter');
+var sh = require('shelljs');
+var uglify = require('gulp-uglify');
 var mainBowerFiles = require('main-bower-files');
-var shelljs = require('shelljs');
+var less = require('gulp-less');
 
-// Include plugins
-var plugins = require("gulp-load-plugins")({
-    pattern: ['gulp-*', 'gulp.*'],
-    replaceString: /\bgulp[\-.]/
-});
+var livereload = require('livereload');
 
-// Define default destination folder
 var paths = {
     sass: ['./scss/**/*.scss'],
-    css: ['./www/css/**/*.css'],
-    js: ['./www/account/**/*.js', './www/home/**/*.js', './www/shared/**/*.js', './www/user/**/*.js']
+    js: ['./www/js/**/*.js', '!./www/js/test/**/*.js'],
+    bower: ['./www/lib', '!./www/lib/dist']
 };
 
-gulp.task('default', ['lib', 'js', 'sass']); //, 'watch'
+gulp.task('default', ['sass', 'js', 'bower-js', 'bower-css', 'livereload']); //, 'watch'
 
-gulp.task('lib', ['libjs', 'libcss']);
-
-gulp.task('libjs', function () {
-    gulp.src(mainBowerFiles())
-        .pipe(plugins.filter('*.js'))
-        .pipe(plugins.concat('lib.min.js'))
-        .pipe(plugins.uglify())
-        .pipe(gulp.dest('./www/js'));
+gulp.task('watch', function () {
+    gulp.watch(paths.sass, ['sass']);
+    gulp.watch(paths.js, ['js']);
+    gulp.watch(paths.bower, ['bower']);
 });
 
-gulp.task('libcss', function () {
-    gulp.src(mainBowerFiles())
-        .pipe(plugins.filter('*.css'))
-        .pipe(plugins.concat('lib.css'))
-        .pipe(plugins.uglify())
-        .pipe(gulp.dest('./www/css'));
+gulp.task('livereload', function () {
+    var server = livereload.createServer();
+    server.watch('./www');
+});
+
+gulp.task('sass', function (done) {
+    gulp.src('./scss/app.scss')
+        .pipe(sass())
+        .on('error', sass.logError)
+        .pipe(gulp.dest('./www/css/'))
+        .pipe(minifyCss({
+            keepSpecialComments: 0
+        }))
+        .pipe(rename({ extname: '.min.css' }))
+        .pipe(gulp.dest('./www/css/'))
+        .on('end', done);
 });
 
 gulp.task('js', function () {
     gulp.src(paths.js)
-        .pipe(plugins.concat('src.js'))
-        .pipe(gulp.dest('./www/js'))
-        .pipe(plugins.rename({ suffix: '.min' }))
-        .pipe(plugins.uglify())
-        .pipe(gulp.dest('./www/js'));
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest('./www/lib'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(uglify())
+        .pipe(gulp.dest('./www/lib'));
 });
 
-gulp.task('sass', function (done) {
-    gulp.src(paths.sass)
-        .pipe(plugins.sass())
-        .on('error', plugins.sass.logError)
-        .pipe(gulp.dest('./www/css'))
-        .pipe(plugins.minifyCss({
-            keepSpecialComments: 0
-        }))
-        .pipe(plugins.rename({ extname: '.min.css' }))
-        .pipe(gulp.dest('./www/css'))
-        .on('end', done);
+gulp.task('bower-js', function () {
+    gulp.src(mainBowerFiles())
+        .pipe(filter('*.js'))
+        .pipe(concat('bower.js'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(uglify())
+        .pipe(gulp.dest('./www/lib'));
 });
 
-gulp.task('watch', function () {
-    gulp.watch(paths.js, ['src']);
-    gulp.watch(paths.sass, ['sass']);
+gulp.task('bower-css', function () {
+    gulp.src(mainBowerFiles())
+        .pipe(filter(['*.less', '*.css']))
+        .pipe(less())
+        .pipe(concat('bower.min.css'))
+        .pipe(minifyCss())
+        .pipe(gulp.dest('./www/lib'));
 });
 
-
-
-/*
 gulp.task('install', ['git-check'], function () {
     return bower.commands.install()
         .on('log', function (data) {
-            plugins.gutil.log('bower', plugins.gutil.colors.cyan(data.id), data.message);
+            gutil.log('bower', gutil.colors.cyan(data.id), data.message);
         });
 });
 
 gulp.task('git-check', function (done) {
-    if (!plugins.sh.which('git')) {
+    if (!sh.which('git')) {
         console.log(
             '  ' + gutil.colors.red('Git is not installed.'),
             '\n  Git, the version control system, is required to download Ionic.',
@@ -91,14 +94,9 @@ gulp.task('git-check', function (done) {
     }
     done();
 });
-*/
 
-/*
-    var css = gulp.src(paths.css)
-        .pipe(plugins.concat('src.css'))
-        .pipe(gulp.dest(dest + 'css'))
-        .pipe(plugins.rename({ suffix: '.min' }))
-        .pipe(plugins.minifyCss())
-        .pipe(gulp.dest(dest + 'css'));
-*/
-//    return plugins.merge(js, css);
+// http://clubmate.fi/bower-and-gulp-match-made-in-heaven-also/
+// gulp.task('bower-test', function() {
+//     return gulp.src(mainBowerFiles())
+//         .pipe(gulp.dest('./www/lib'))
+// });
