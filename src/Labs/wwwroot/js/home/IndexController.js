ae7e19dd376b828e896b3fbe4438cbd2fde0cc1f
warrendodsworth <warren.dodsworth@gmail.com>
2016-02-25 15:07:41 +1100

    Setup net5 project

---
+++ src/Labs/wwwroot/js/home/IndexController.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    'use strict';
+
+    //Index
+    angular.module('controllers')
+           .controller('IndexController', IndexController);
+
+    IndexController.$inject = ['$scope', '$http', '$location'];
+
+    function IndexController($scope, $http, $location) {
+
+        $http.get('/api/notes').then(function (res) {
+            $scope.notes = res.data;
+        });
+
+    }
+})();
---
+++ src/Labs/wwwroot/js/shared/NavbarController.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    'use strict';
+
+    angular
+      .module('controllers')
+      .controller('NavbarController', ['$scope', '$location', 'AccountService', NavbarController]);
+
+    function NavbarController($scope, $location, AccountService) {
+
+        $scope.identity = AccountService.identity;
+
+        $scope.logout = function () {
+            AccountService.logout();
+            $location.path('/');
+        }
+    }
+})();
+
+
+//$scope.$on('user:logout', function (event, data) {
+//    $scope.identity = data;
+//});
