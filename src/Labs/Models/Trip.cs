dd5403c51bc92a893809af9c81ca7409265128e2
warrendodsworth <warren.dodsworth@gmail.com>
2016-03-02 10:21:39 +1100

    add labs project

---
+++ src/Labs/Models/Trip.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Labs.Models
+{
+  public class Trip
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public DateTime Created { get; set; }
+    public string UserName { get; set; }
+
+    public IList<Stop> Stops { get; set; }
+  }
+
+  public class Stop
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Longitude { get; set; }
+    public double Latitude { get; set; }
+    public DateTime Arrival { get; set; }
+  }
+}
