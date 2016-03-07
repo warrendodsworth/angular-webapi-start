b03b96f82899b5a60b3680d9846de185ad018dbd
warrendodsworth <warren.dodsworth@gmail.com>
2016-03-08 09:43:07 +1100

    add angular sanitize for $sce

--- src/Api/wwwroot/_references.js
+++ src/Api/wwwroot/_references.js
@@ -16,5 +16,6 @@
 /// <reference path="lib/angular/angular.js" />
 /// <reference path="lib/angular-bootstrap/ui-bootstrap-tpls.js" />
 /// <reference path="lib/angular-route/angular-route.js" />
+/// <reference path="lib/angular-sanitize/angular-sanitize.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
