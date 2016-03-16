02b1c9d2cbfa274340adc3139ca96bffceca80fe
warrendodsworth <warren.dodsworth@gmail.com>
2016-03-16 12:07:48 +1100

    add seed user adjust routes

--- Api/Migrations/Configuration.cs
+++ Api/Migrations/Configuration.cs
@@ -1,31 +1,26 @@
 namespace Api.Migrations
 {
-    using System;
-    using System.Data.Entity;
-    using System.Data.Entity.Migrations;
-    using System.Linq;
-
-    internal sealed class Configuration : DbMigrationsConfiguration<Api.Models.AppContext>
+  using System;
+  using System.Data.Entity;
+  using System.Data.Entity.Migrations;
+  using System.Linq;
+  using Microsoft.AspNet.Identity;
+  using Models;
+  internal sealed class Configuration : DbMigrationsConfiguration<Api.Models.AppContext>
+  {
+    public Configuration()
     {
-        public Configuration()
-        {
-            AutomaticMigrationsEnabled = true;
-        }
-
-        protected override void Seed(Api.Models.AppContext context)
-        {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
-        }
+      AutomaticMigrationsEnabled = true;
     }
+
+    protected override void Seed(Api.Models.AppContext db)
+    {
+      var UserManager = new UserManager(db);
+      var user = UserManager.FindByName("user");
+      if(user == null)
+      {
+        UserManager.Create(new User { Name = "Test", UserName = "user" }, "password");
+      }
+    }
+  }
 }
--- Api/js/app-routes.js
+++ Api/js/app-routes.js
@@ -7,20 +7,20 @@
       .config(['$routeProvider', function ($routeProvider) {
 
         //Account
-        $routeProvider.when('/login', { templateUrl: 'js/account/login.html', controller: 'LoginController' })
-            .when('/register', { templateUrl: 'js/account/register.html', controller: 'RegisterController' })
-            .when('/manage', { templateUrl: 'js/account/manage/manage.html', controller: 'ManageController' })
-            .when('/manage/logins', { templateUrl: 'js/account/manage/manageLogins.html', controller: 'ManageLoginsController' });
+        $routeProvider.when('/login', { templateUrl: '/js/account/login.html', controller: 'LoginController' })
+            .when('/register', { templateUrl: '/js/account/register.html', controller: 'RegisterController' })
+            .when('/manage', { templateUrl: '/js/account/manage/manage.html', controller: 'ManageController' })
+            .when('/manage/logins', { templateUrl: '/js/account/manage/manageLogins.html', controller: 'ManageLoginsController' });
 
         //User
         $routeProvider
-          .when('/notes/create', { templateUrl: 'js/user/notes/create.html', controller: 'Notes.CreateController' });
+          .when('/notes/create', { templateUrl: '/js/user/notes/create.html', controller: 'Notes.CreateController' });
 
         $routeProvider
-          .when('/facebook', { templateUrl: 'js/user/facebook/facebook.html', controller: 'FacebookController' });
+          .when('/facebook', { templateUrl: '/js/user/facebook/facebook.html', controller: 'FacebookController' });
 
         $routeProvider
-          .when('/', { templateUrl: 'js/home/index.html', controller: 'IndexController' })
+          .when('/', { templateUrl: '/js/home/index.html', controller: 'IndexController' })
           .otherwise({ redirectTo: '/' });
 
       }]);
