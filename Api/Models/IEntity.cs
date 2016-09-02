a56db37554adc7df97f6a1cd4629cc746dbee2ee
warrendodsworth <warren@jobadder.com>
2016-09-02 12:35:34 +1000

    update seed method, update datetime2 col

--- Api/Models/IEntity.cs
+++ Api/Models/IEntity.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -20,6 +21,7 @@ namespace Api.Models
 
     public string CreateBy { get; set; }
 
+    [Column(TypeName = "datetime2")]
     public DateTime CreateDate { get; set; }
 
     [Timestamp]
