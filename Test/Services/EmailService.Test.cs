34f013aaace3cd97eaa8e8f78268dee7a6b94f76
warrendodsworth <warren@jobadder.com>
2016-09-12 12:28:27 +1000

    add email service shell & tests

---
+++ Test/Services/EmailService.Test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test.Services
+{
+  [TestClass]
+  public class EmailServiceTest
+  {
+    [TestInitialize]
+    public void Init()
+    {
+    }
+
+    [TestMethod]
+    public void Email_SendMailTemplate_ShouldSend()
+    {
+
+
+      Assert.IsFalse(false);
+    }
+  }
+}
