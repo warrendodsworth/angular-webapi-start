905d5494f99622d62ed4828a4e1b464560efd46b
warrendodsworth <warren@jobadder.com>
2016-10-10 13:47:27 +1100

    add labs endpoint to return list of enums

--- Api/Controllers/Labs/LabsController.cs
+++ Api/Controllers/Labs/LabsController.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Api.Models;
+using Api.Models.Enums;
+using Api.Services.Helpers;
+using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Web.Http;
 
@@ -7,12 +11,37 @@ namespace Api.Controllers.Labs
   [RoutePrefix("api/labs")]
   public class LabsController : ApiController
   {
-    [Route("")]
-    public IHttpActionResult Get()
-    {
-      var baseUrl = Request.RequestUri.GetLeftPart(UriPartial.Authority);
+	[Route("")]
+	public IHttpActionResult Get()
+	{
+	  var baseUrl = Request.RequestUri.GetLeftPart(UriPartial.Authority);
 
-      return Redirect(baseUrl + "/#/login");
-    }
+	  return Redirect(baseUrl + "/#/login");
+	}
+
+	[Route("users/status-list")]
+	public IHttpActionResult GetUserStatusList()
+	{
+	  var enumVals = new List<object>();
+	  foreach (var item in Enum.GetValues(typeof(UserStatus)))
+	  {
+		enumVals.Add(new { id = (int)item, name = item.ToString() });
+	  }
+
+	  return Ok(enumVals);
+	}
+
+	[Route("us-timezones")]
+	public IHttpActionResult GetTimeZonesJson()
+	{
+	  var enumVals = new List<object>();
+
+	  foreach (var item in Enum.GetValues(typeof(USTimeZones)))
+	  {
+		enumVals.Add(new { id = (int)item, name = EnumHelper.GetDescription<USTimeZones>(item.ToString()), value = item.ToString() });
+	  }
+
+	  return Ok(enumVals);
+	}
   }
 }
---
+++ Api/Models/Enums/USTimeZones.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Api.Models.Enums
+{
+  public enum USTimeZones
+  {
+	[Description("Alaskan Daylight Time")]
+	AlaskanDaylightTime,
+	AlaskanStandardTime,
+	CentralDaylightTime,
+	CentralStandardTime,
+	EasternDaylightTime,
+	EasternStandardTime,
+	HawaiianStandardTime,
+	MountainDaylightTime,
+	MountainStandardTime,
+	MountainTime,
+	PacificDaylightTime,
+	PacificStandardTime,
+	USEasternStandardTime,
+	USMountainStanardTime
+  };
+}
