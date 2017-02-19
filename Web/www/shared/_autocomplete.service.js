23ae215c95055a07efb4ea460dcc318f5cadc42b
warrendodsworth <warrendodsworth@outlook.com>
2017-02-20 10:34:40 +1100

    add autocomplete service, add getAddressHint

---
+++ Web/www/shared/_autocomplete.service.js
@@ -0,0 +1,26 @@
+﻿(function () {
+  'use strict';
+
+  angular
+    .module('serices')
+    .factory('_autocomplete', ['$http', _autocomplete]);
+
+  function _autocomplete($http) {
+    var service = {};
+
+    service.getAddressHint = function (val) {
+      return $http.get('//maps.googleapis.com/maps/api/geocode/json', {
+        params: {
+          address: val,
+          sensor: false
+        }
+      }).then(function (response) {
+        return response.data.results.map(function (item) {
+          return item.formatted_address;
+        });
+      });
+    };
+
+    return service;
+  }
+})();
