1e677ae2f357355690e95c0f3b8fdae31d71c0c7
warrendodsworth <warrendodsworth@outlook.com>
2017-02-20 10:03:42 +1100

    edit navbar typeahead open + search on focus

---
+++ Web/www/shared/dir/focusMe.js
@@ -0,0 +1,29 @@
+﻿(function () {
+  'use strict';
+
+  angular
+  .module('app')
+  .directive('focusMe', function ($timeout, $parse) {
+    return {
+      //focus an element on variable change
+      //scope: true,   // optionally create a child scope
+      link: function (scope, element, attrs) {
+        var model = $parse(attrs.focusMe);
+        scope.$watch(model, function (value) {
+          if (value === true) {
+            $timeout(function () {
+              element[0].focus();
+            });
+          }
+        });
+      }
+    };
+  });
+})();
+
+
+//vm.onFocus = function (e) {
+//  $timeout(function () {
+//    $(e.target).trigger('input');
+//  });
+//};
--- Web/www/shared/navbar.directive.js
+++ Web/www/shared/navbar.directive.js
@@ -1,5 +1,5 @@
 (function () {
-  'use strict'; 
+  'use strict';
 
   angular
     .module('app')
@@ -25,57 +25,10 @@
     };
 
     vm.getPosts = function (q) {
-      return $http.get('api/posts?search=' + q).then(function (res) {
-        return res.data;
-      });
-    };
-
-    vm.onFocus = function (e) {
-      $timeout(function () {
-        $(e.target).trigger('input');
+      return $http.get('api/posts?search=' + (q || '') + '&show=6').then(function (res) {
+        return res.data.items;
       });
     };
   }
-
-
-
-  angular
-    .module('app')
-  .directive('focusMe', function ($timeout, $parse) {
-    return {
-      //scope: true,   // optionally create a child scope
-      link: function (scope, element, attrs) {
-        var model = $parse(attrs.focusMe);
-        scope.$watch(model, function (value) {
-          if (value === true) {
-            $timeout(function () {
-              element[0].focus();
-              console.log('focusMe');
-            });
-          }
-        });
-      }
-    };
-  })
-  .directive('emptyTypeahead', function () {
-    return {
-      require: 'ngModel',
-      link: function (scope, element, attrs, modelCtrl) {
-        var secretEmptyKey = '[$empty$]';
-
-        // this parser run before typeahead's parser
-        modelCtrl.$parsers.unshift(function (inputValue) {
-          var value = (inputValue ? inputValue : secretEmptyKey); // replace empty string with secretEmptyKey to bypass typeahead-min-length check
-          modelCtrl.$viewValue = value; // this $viewValue must match the inputValue pass to typehead directive
-          return value;
-        });
-
-        // this parser run after typeahead's parser
-        modelCtrl.$parsers.push(function (inputValue) {
-          return inputValue === secretEmptyKey ? '' : inputValue; // set the secretEmptyKey back to empty string
-        });
-      }
-    }
-  });
 
 }());
