3884ce2d8eaa7f27f066b7113808627ac0719a3c
warrendodsworth <warrendodsworth@outlook.com>
2017-03-01 10:42:33 +1100

    add return url redirect

--- Web/www/account/login.controller.js
+++ Web/www/account/login.controller.js
@@ -1,16 +1,14 @@
 (function () {
   'use strict';
 
-  angular.module('controllers').controller('LoginController', [
-    '$scope',
-    '$location',
-    '_account',
-    LoginController
-  ]);
+  angular.module('controllers').controller('LoginController', LoginController);
+
+  LoginController.$inject = ['$scope', '$location', '_account'];
 
   function LoginController($scope, $location, _account) {
     var vm = $scope;
     vm.$parent.title = 'Wow';
+    var returnUrl = $location.search()['returnUrl'];
 
     _account.getExternalLogins().then(function (res) {
       vm.logins = res.data;
@@ -18,6 +16,11 @@
 
     vm.login = function (model) {
       _account.login(model).then(function (res) {
+        if (returnUrl) {
+          $location.path(returnUrl).search({});
+          return;
+        }
+
         $location.path('/').search('m', 'welcome');
       });
     };
