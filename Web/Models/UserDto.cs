fcda18762dac44e75351176a4423d3bc01d3f6e0
warrendodsworth <warrendodsworth@outlook.com>
2017-03-01 10:45:36 +1100

    edit json property username

--- Web/Models/UserDto.cs
+++ Web/Models/UserDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
@@ -7,6 +8,7 @@ namespace Web.Models
   public class UserDto : IValidatableObject
   {
     public string Name { get; set; }
+    [JsonProperty("username")]
     public string UserName { get; set; }
     public string Email { get; set; }
     public string PhotoUrl { get; set; }
