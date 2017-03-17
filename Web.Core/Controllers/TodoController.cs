0f2d493321f701835847c599bac38225b8185cbf
warrendodsworth <warrendodsworth@outlook.com>
2017-03-17 12:03:10 +1100

    Add dotnet core proj

---
+++ Web.Core/Controllers/TodoController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TodoApi.Models;
+using Web.Services;
+
+namespace Web.Controllers
+{
+  [Route("api/[controller]")]
+  public class TodoController : Controller
+  {
+    private ITodoService _service;
+
+    public TodoController(ITodoService service)
+    {
+      _service = service;
+    }
+
+    [HttpGet]
+    public IEnumerable<TodoItem> GetAll()
+    {
+      return _service.GetAll();
+    }
+
+    [HttpGet("{id}", Name = "GetTodo")]
+    public IActionResult GetById(long id)
+    {
+      var item = _service.Find(id);
+      if (item == null) {
+        return NotFound();
+      }
+      return new ObjectResult(item);
+    }
+
+    [HttpPost]
+    public IActionResult Create([FromBody] TodoItem item)
+    {
+      if (item == null) {
+        return BadRequest();
+      }
+
+      _service.Add(item);
+
+      return CreatedAtRoute("GetTodo", new { id = item.Key }, item);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Update(long id, [FromBody] TodoItem item)
+    {
+      if (item == null || item.Key != id) {
+        return BadRequest();
+      }
+
+      var todo = _service.Find(id);
+      if (todo == null) {
+        return NotFound();
+      }
+
+      todo.IsComplete = item.IsComplete;
+      todo.Name = item.Name;
+
+      _service.Update(todo);
+      return new NoContentResult();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(long id)
+    {
+      var todo = _service.Find(id);
+      if (todo == null) {
+        return NotFound();
+      }
+
+      _service.Remove(id);
+      return new NoContentResult();
+    }
+  }
+}
---
+++ Web.Core/Services/TodoService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoApi.Models;
+
+namespace Web.Services
+{
+  public interface ITodoService
+  {
+    void Add(TodoItem item);
+    IEnumerable<TodoItem> GetAll();
+    TodoItem Find(long key);
+    void Remove(long key);
+    void Update(TodoItem item);
+  }
+
+  public class TodoService : ITodoService
+  {
+    private readonly TodoContext _context;
+
+    public TodoService(TodoContext context)
+    {
+      _context = context;
+      Add(new TodoItem { Name = "Item todo, think about it" });
+      Add(new TodoItem { Name = "Do it again" });
+    }
+
+    public IEnumerable<TodoItem> GetAll()
+    {
+      return _context.TodoItems.ToList();
+    }
+
+    public void Add(TodoItem item)
+    {
+      _context.TodoItems.Add(item);
+      _context.SaveChanges();
+    }
+
+    public TodoItem Find(long key)
+    {
+      return _context.TodoItems.FirstOrDefault(t => t.Key == key);
+    }
+
+    public void Remove(long key)
+    {
+      var entity = _context.TodoItems.First(t => t.Key == key);
+      _context.TodoItems.Remove(entity);
+      _context.SaveChanges();
+    }
+
+    public void Update(TodoItem item)
+    {
+      _context.TodoItems.Update(item);
+      _context.SaveChanges();
+    }
+  }
+}
